7d1cf9a7fa3b3f6672c9952b86b202e9c9eeb66e
guoyajie <guoyajie@mallog.cn>
2014-07-13 22:00:01 +0800

    Create found2.swift

---
+++ found2.swift
@@ -0,0 +1,31 @@
+//断言
+let age=-3
+assert(age>=0,"A person's age cannot be less than zero")
+assert(age>=0)
+//何时使用断言 
+
+//2.2基本运算符
+let i=1+2
+if enteredDoorCode&&passedRetinaScan
+++j
+//可以对浮点数进行取余运算(%)
+a..b
+a...b
+
+//术语
+//一目运算符（前置 后置） -a !b i++
+//双目运算符（中置） a+b
+//三目运算符 a?b:c
+
+//赋值运算符
+let b=10
+var a=5
+a=b
+let (x,y)=(1,2)
+
+//数值运算
+1+2
+"hello, "+"world"
+let dog:Character="d"
+let cow:Character="c"
+let dogCow=dog+cow
