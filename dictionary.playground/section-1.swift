2cfc14c97cf6c0f4bc0ce2f66812e0cb0486f47d
guoyajie <guoyajie@mallog.cn>
2014-07-14 17:12:28 +0800

    ggg

---
+++ dictionary.playground/section-1.swift
@@ -0,0 +1,5 @@
+// Playground - noun: a place where people can play
+var airports=["TYO":"Tokyo","DUB":"Dublin"]
+for (airportCode,airportName) in airports{
+    println("\(airportCode): \(airportName)")
+}
